44949cd7131d314d418d0bcf408ca5bacf51bbc2
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-02-11 20:15:42 +0600

    Resolved Add top podcasters section UI #7 (#26)
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * extra bg-color removed from container
    
    * Delete .github/workflows/main.yml
    
    * Add search section UI Issue #4
    
    * ui: updated some design
    
    * Top podcasters section completed
    
    * Resolved Add top podcasters section UI #7
    
    ---------
    
    Co-authored-by: Shakil Ahmed <shakilahmmeed@gmail.com>

--- src/components/TopPodcasters.tsx
+++ src/components/TopPodcasters.tsx
@@ -1,9 +1,66 @@
 import React from "react";
 
 export default function TopPodcasters() {
-	return (
-		<div className="rounded-xl h-[300px] bg-gray-100 flex items-center justify-center">
-			TopPodcasters
-		</div>
-	);
+  const podcastersList = [
+    {
+      id: 1,
+      img: "https://mdbcdn.b-cdn.net/img/new/avatars/2.webp",
+      artist: "Joe Rogan",
+      followers: "407,109",
+      following: false,
+    },
+    {
+      id: 2,
+      img: "https://mdbcdn.b-cdn.net/img/new/avatars/2.webp",
+      artist: "Andrew Klavan",
+      followers: "321,826",
+      following: false,
+    },
+    {
+      id: 3,
+      img: "https://mdbcdn.b-cdn.net/img/new/avatars/2.webp",
+      artist: "Joyce Meyer",
+      followers: "266,001",
+      following: true,
+    },
+  ];
+
+  return (
+    <div className="rounded-xl flex flex-col p-5">
+      <h4 className="uppercase font-semibold">Top Podcasters</h4>
+      <div className="">
+        {podcastersList.map((item) => (
+          <div
+            key={item.id}
+            className="podcaster mt-4 flex flex-row justify-around items-center"
+          >
+            <div className="img">
+              <img
+                src={item.img}
+                alt="user-photo"
+                className="w-12 h-12 rounded-full"
+              />
+            </div>
+            <div className="info">
+              <div className="name font-bold text-sm">{item.artist}</div>
+              <div className="follower text-xs text-gray-500">
+                {item.followers} followers
+              </div>
+            </div>
+            <div className="follow">
+              <button
+                className={`text-xs  font-bold  rounded-full cursor-pointer w-20 py-1.5 ${
+                  item.following
+                    ? "bg-blue-500 text-white"
+                    : "bg-blue-200 text-blue-600"
+                }`}
+              >
+                {item.following ? "Following" : "Follow"}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
