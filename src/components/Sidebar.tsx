7e7c74e123f23f46ca6445abdc259c0b9148cab1
Rafiad Hossan <78840816+SayedRafiadHossan@users.noreply.github.com>
2024-02-29 21:03:15 +0600

    Add icon in slider (#32)
    * Add icon in slider
    
    * Add icons in sidebar
    
    * Update SidebarMenu.tsx
    
    ---------
    
    Co-authored-by: Rafiad <rafiadhoseen@gmail.com>
    Co-authored-by: Shakil Ahmed <shakilahmmeed@gmail.com>

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,99 +2,77 @@
 "use client";
 import React, { useState } from "react";
 import { Switch } from "antd";
-import { useRouter } from "next/navigation";
-
-const menuItems = [
-  { label: "Discover", path: "/discover" },
-  { label: "Trending", path: "/trending" },
-  { label: "Genres", path: "/genres" },
-  { label: "Albums", path: "/albums" },
-  { label: "Speaker", path: "/speaker" },
-];
-
-const libraryItems = [
-  { label: "Recent", path: "/recent" },
-  { label: "Playlists", path: "/playlists" },
-  { label: "Favourites", path: "/favourites" },
-  { label: "Local", path: "/local" },
-];
-
-const playlistItems = [
-  { label: "New playlist", path: "/new-playlist" },
-  { label: "Import playlist", path: "/import-playlist" },
-  { label: "Account", path: "/account" },
-  { label: "Settings", path: "/settings" },
-];
+import { menuItems, libraryItems, customizeItems } from "./SidebarMenu.tsx";
 
 export default function Sidebar() {
-  const router = useRouter();
-  const [checked, setChecked] = useState(true);
-  const handleClick = () => {
-    setChecked((p) => !p);
-  };
-  return (
-    <div className="w-[250px] h-screen bg-[#fbfbfb] p-5">
-      <button
-        onClick={() => router.push("/home")}
-        className="font-medium text-xl text-center"
-      >
-        Groovy
-      </button>
-      <div className="flex flex-col gap-5 mt-16">
-        <div>
-          <h2 className="mb-2">MENU</h2>
-          <div>
-            {menuItems.map((item, idx) => (
-              <MenuItem key={idx} {...item} />
-            ))}
-          </div>
-        </div>
-        <div>
-          <h2 className="mb-2">LIBRARY</h2>
-          <div>
-            {libraryItems.map((item, idx) => (
-              <MenuItem key={idx} {...item} />
-            ))}
-          </div>
-        </div>
-        <div>
-          <h2 className="mb-2">PLAYLIST</h2>
-          <div>
-            {playlistItems.map((item, idx) => (
-              <MenuItem key={idx} {...item} />
-            ))}
-          </div>
-        </div>
-        <div>
-          <div className="flex gap-2 items-center">
-            <Switch
-              className="w-[50px]"
-              defaultChecked
-              checked={checked}
-              onClick={handleClick}
-            />
-            {checked ? (
-              <p className="text-gray-500 text-sm">Black</p>
-            ) : (
-              <p className="text-gray-500 text-sm">Light</p>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const router = useRouter();
+	const [checked, setChecked] = useState(true);
+	const handleClick = () => {
+		setChecked((p) => !p);
+	};
+	return (
+		<div className="w-[250px] h-screen bg-[#fbfbfb] p-5">
+			<button
+				onClick={() => router.push("/home")}
+				className="font-medium text-xl text-center"
+			>
+				Groovy
+			</button>
+			<div className="flex flex-col gap-5 mt-16">
+				<div>
+					<h2 className="mb-2">MENU</h2>
+					<div>
+						{menuItems.map((item, idx) => (
+							<MenuItem key={idx} {...item} />
+						))}
+					</div>
+				</div>
+				<div>
+					<h2 className="mb-2">LIBRARY</h2>
+					<div>
+						{libraryItems.map((item, idx) => (
+							<MenuItem key={idx} {...item} />
+						))}
+					</div>
+				</div>
+				<div>
+					<h2 className="mb-2">CUSTOMIZE</h2>
+					<div>
+						{customizeItems.map((item, idx) => (
+							<MenuItem key={idx} {...item} />
+						))}
+					</div>
+				</div>
+				<div>
+					<div className="flex gap-2 items-center">
+						<Switch
+							className="w-[50px]"
+							defaultChecked
+							checked={checked}
+							onClick={handleClick}
+						/>
+						{checked ? (
+							<p className="text-gray-500 text-sm">Black</p>
+						) : (
+							<p className="text-gray-500 text-sm">Light</p>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-const MenuItem = ({ label, path }) => {
-  const router = useRouter();
-  return (
-    <div>
-      <button
-        className="font-medium px-3 py-1.5 text-sm hover:bg-gray-200 rounded cursor-pointer"
-        onClick={() => router.push(path)}
-      >
-        {label}
-      </button>
-    </div>
-  );
+const MenuItem = ({ label, icon, path }) => {
+	const router = useRouter();
+
+	return (
+		<div
+			className="flex items-center gap-3 font-medium px-3 py-1.5 text-sm hover:bg-gray-200 rounded cursor-pointer"
+			onClick={() => router.push(path)}
+		>
+			{icon}
+			{label}
+		</div>
+	);
 };
---
+++ src/components/SidebarMenu.tsx
@@ -0,0 +1,350 @@
+export const menuItems = [
+	{
+		label: "Explore",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M17 3.34a10 10 0 1 1 -15 8.66l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 14.66a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm3.684 -10.949l-6 2a1 1 0 0 0 -.633 .633l-2.007 6.026l-.023 .086l-.017 .113l-.004 .068v.044l.009 .111l.012 .07l.04 .144l.045 .1l.054 .095l.064 .09l.069 .075l.084 .074l.098 .07l.1 .054l.078 .033l.105 .033l.109 .02l.043 .005l.068 .004h.044l.111 -.009l.07 -.012l.02 -.006l.019 -.002l.074 -.022l6 -2a1 1 0 0 0 .633 -.633l2 -6a1 1 0 0 0 -1.265 -1.265zm-1.265 2.529l-1.21 3.629l-3.629 1.21l1.21 -3.629l3.629 -1.21zm-9.419 1.42a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm14 0a1 1 0 1 0 0 2a1 1 0 0 0 0 -2zm-7 -7a1 1 0 1 0 0 2a1 1 0 0 0 0 -2z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/discover",
+	},
+	{
+		label: "Trending",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M18 3a3 3 0 0 1 3 3v12a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12zm-2.293 6.293a1 1 0 0 0 -1.414 0l-2.293 2.292l-1.293 -1.292a1 1 0 0 0 -1.414 0l-3 3a1 1 0 0 0 0 1.414l.094 .083a1 1 0 0 0 1.32 -.083l2.293 -2.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -.083l2.293 -2.292l1.293 1.292a1 1 0 0 0 1.414 -1.414l-2 -2z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/trending",
+	},
+	{
+		label: "Genres",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005h5z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+				<path d="M19 7h-4l-.001 -4.001z" strokeWidth="0" fill="currentColor" />
+			</svg>
+		),
+		path: "/genres",
+	},
+	{
+		label: "Albums",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M9 3a1 1 0 0 1 .608 .206l.1 .087l2.706 2.707h6.586a3 3 0 0 1 2.995 2.824l.005 .176v8a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-11a3 3 0 0 1 2.824 -2.995l.176 -.005h4z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/albums",
+	},
+	{
+		label: "Speaker",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="2"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					fill="black"
+					d="M9 2m0 3a3 3 0 0 1 3 -3h0a3 3 0 0 1 3 3v5a3 3 0 0 1 -3 3h0a3 3 0 0 1 -3 -3z"
+				/>
+				<path d="M5 10a7 7 0 0 0 14 0" />
+				<path d="M8 21l8 0" />
+				<path d="M12 17l0 4" />
+			</svg>
+		),
+		path: "/speaker",
+	},
+];
+
+export const libraryItems = [
+	{
+		label: "Recent",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M9.883 2.207a1.9 1.9 0 0 1 2.087 1.522l.025 .167l.005 .104v7a1 1 0 0 0 .883 .993l.117 .007h6.8a2 2 0 0 1 2 2a1 1 0 0 1 -.026 .226a10 10 0 1 1 -12.27 -11.933l.27 -.067l.11 -.02z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+				<path
+					d="M14 3.5v5.5a1 1 0 0 0 1 1h5.5a1 1 0 0 0 .943 -1.332a10 10 0 0 0 -6.11 -6.111a1 1 0 0 0 -1.333 .943z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/recent",
+	},
+	{
+		label: "Playlists",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/playlists",
+	},
+	{
+		label: "Favorites",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/favourites",
+	},
+	{
+		label: "Local",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					fill="black"
+					d="M7 21h10a2 2 0 0 0 2 -2v-14a2 2 0 0 0 -2 -2h-6.172a2 2 0 0 0 -1.414 .586l-3.828 3.828a2 2 0 0 0 -.586 1.414v10.172a2 2 0 0 0 2 2z"
+				/>
+				<path stroke="white" d="M13 6v2" />
+				<path stroke="white" d="M16 6v2" />
+				<path stroke="white" d="M10 7v1" />
+			</svg>
+		),
+		path: "/local",
+	},
+];
+
+export const customizeItems = [
+	{
+		label: "New Playlist",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M12 2l.324 .001l.318 .004l.616 .017l.299 .013l.579 .034l.553 .046c4.785 .464 6.732 2.411 7.196 7.196l.046 .553l.034 .579c.005 .098 .01 .198 .013 .299l.017 .616l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.464 4.785 -2.411 6.732 -7.196 7.196l-.553 .046l-.579 .034c-.098 .005 -.198 .01 -.299 .013l-.616 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.785 -.464 -6.732 -2.411 -7.196 -7.196l-.046 -.553l-.034 -.579a28.058 28.058 0 0 1 -.013 -.299l-.017 -.616c-.003 -.21 -.005 -.424 -.005 -.642l.001 -.324l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.464 -4.785 2.411 -6.732 7.196 -7.196l.553 -.046l.579 -.034c.098 -.005 .198 -.01 .299 -.013l.616 -.017c.21 -.003 .424 -.005 .642 -.005zm0 6a1 1 0 0 0 -1 1v2h-2l-.117 .007a1 1 0 0 0 .117 1.993h2v2l.007 .117a1 1 0 0 0 1.993 -.117v-2h2l.117 -.007a1 1 0 0 0 -.117 -1.993h-2v-2l-.007 -.117a1 1 0 0 0 -.993 -.883z"
+					fill="currentColor"
+					strokeWidth="0"
+				/>
+			</svg>
+		),
+		path: "/new-playlist",
+	},
+	{
+		label: "Import Playlist",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M14 3v4a1 1 0 0 0 1 1h4" />
+				<path
+					fill="black"
+					d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"
+				/>
+				<path stroke="white" d="M12 11v6" />
+				<path stroke="white" d="M9.5 13.5l2.5 -2.5l2.5 2.5" />
+			</svg>
+		),
+		path: "/import-playlist",
+	},
+	{
+		label: "Account",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+				<path
+					d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/account",
+	},
+	{
+		label: "Settings",
+		icon: (
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				className="icon icon-tabler icon-tabler-compass-filled"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				strokeWidth="1.5"
+				stroke="currentColor"
+				fill="none"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M14.647 4.081a.724 .724 0 0 0 1.08 .448c2.439 -1.485 5.23 1.305 3.745 3.744a.724 .724 0 0 0 .447 1.08c2.775 .673 2.775 4.62 0 5.294a.724 .724 0 0 0 -.448 1.08c1.485 2.439 -1.305 5.23 -3.744 3.745a.724 .724 0 0 0 -1.08 .447c-.673 2.775 -4.62 2.775 -5.294 0a.724 .724 0 0 0 -1.08 -.448c-2.439 1.485 -5.23 -1.305 -3.745 -3.744a.724 .724 0 0 0 -.447 -1.08c-2.775 -.673 -2.775 -4.62 0 -5.294a.724 .724 0 0 0 .448 -1.08c-1.485 -2.439 1.305 -5.23 3.744 -3.745a.722 .722 0 0 0 1.08 -.447c.673 -2.775 4.62 -2.775 5.294 0zm-2.647 4.919a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z"
+					strokeWidth="0"
+					fill="currentColor"
+				/>
+			</svg>
+		),
+		path: "/settings",
+	},
+];
